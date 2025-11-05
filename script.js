65d5f2c060e7c2a3502033d749e043cc04457857
Igor Araújo <igoritoaraujo223@gmail.com>
2025-11-05 13:48:26 -0300

    SAVE

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-// Pegando o botão e o menu
 const menuBtn = document.getElementById('menu-btn');
 const navMenu = document.getElementById('nav-menu');
+const menuIcon = menuBtn.querySelector('i'); // pega o ícone interno
 
-// Ao clicar no botão, o menu aparece ou desaparece
 menuBtn.addEventListener('click', () => {
   navMenu.classList.toggle('show');
+  menuBtn.classList.toggle('active');
+
+  // Troca o ícone com base no estado
+  if (menuIcon.classList.contains('fa-bars')) {
+    menuIcon.classList.remove('fa-bars');
+    menuIcon.classList.add('fa-xmark');
+  } else {
+    menuIcon.classList.remove('fa-xmark');
+    menuIcon.classList.add('fa-bars');
+  }
 });
